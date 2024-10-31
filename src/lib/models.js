b4aad1d536cfa44b7d400210d222e024a880777e
FALSAL <coconto.info@gmail.com>
2024-10-31 09:23:44 +0100

    deploying mongoose: connection, models and data retrieval functions

---
+++ src/lib/models.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        min: 3,
+        max: 20,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        max: 50,
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 6,
+    },
+    img:{
+        type: String
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    }
+}, 
+{timeStamps: true}
+)
+
+
+const postSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    desc:{
+        type: String,
+        required: true,
+        max: 550,
+    },
+    img:{
+        type: String
+    },
+    userId:{
+        type: String,
+        required: true,
+    },
+    slug:{
+        type: String,
+        required: true,
+        unique: true,
+    }
+}, 
+{timeStamps: true}
+)
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
+export const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
