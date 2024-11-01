05b0d498332ef40d56ed2bef6a76d1b6eb62531c
FALSAL <coconto.info@gmail.com>
2024-11-01 14:58:33 +0100

    applying noStore for caching in getUsers()

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,6 @@
 import { Post, User } from "./models";
 import { connectToDb } from "./utils";
+import { unstable_noStore as noStore } from "next/cache";
 
 
 
@@ -37,6 +38,8 @@ export const getPost = async (slug)=>{
 }
 
 export const getUsers = async ()=>{
+  noStore();
+  
   try {
     connectToDb();
     const users = await User.find(); 
