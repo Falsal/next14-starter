0773e0c3e55158343e492beb7393042342a32153
FALSAL <coconto.info@gmail.com>
2024-11-02 11:42:35 +0100

    connect action function with form input values, basic use

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,5 +1,18 @@
-export const sayHello = async ()=>{
+export const addPost = async (formData)=>{
     "use server"
 
-    console.log("Hello");
+    // basic method for extracting form input values
+    // const title = formData.get("title")
+    // const desc = formData.get("desc")
+    // const slug = formData.get("slug")
+    // const userId = formData.get("userId")
+    
+    // restructuring method
+    const {title, desc, slug, userId} = Object.fromEntries(formData);
+
+    console.log(title, desc, slug, userId)
+
+
+    
+    
 }
