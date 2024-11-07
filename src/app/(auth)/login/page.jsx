1705c094f98ed1eba55d90ee01f055cd05a94906
FALSAL <coconto.info@gmail.com>
2024-11-07 20:56:44 +0100

    set up login page with lib/auth . Note : had to re-install next-auth due to url parsing error. now the github auth page shows up

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,21 @@
-const Loginpage = () => {
+import { auth, signIn } from "@/lib/auth"
+
+const LoginPage = async () => {
+  
+  const session = auth();
+  console.log("Session:")
+  console.log(session)
+  const handleGithubLogin = async ()=>{
+    "use server"
+    await signIn("github");
+  }
   return (
-    <div>Loginpage</div>
+    <div>
+      <form action={handleGithubLogin}>
+        <button>Login with Github</button>
+      </form>
+    </div>
   )
 }
 
-export default Loginpage
+export default LoginPage
