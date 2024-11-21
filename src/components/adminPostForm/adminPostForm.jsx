6e9e0035c8f1a66a7d0368df1852c064888601f6
FALSAL <coconto.info@gmail.com>
2024-11-21 19:09:08 +0100

    Admin page: create user and post forms' functionality

--- src/components/adminPostForm/adminPostForm.jsx
+++ src/components/adminPostForm/adminPostForm.jsx
@@ -1,8 +1,24 @@
-import styles from "./adminPostForm.module.css";
+"use client"
 
-const AdminPostForm = () => {
+import { addPost } from "@/lib/action";
+import styles from "./adminPostForm.module.css";
+import {useActionState} from"react";
+
+const AdminPostForm = ({userId}) => {
+  const [state, formAction] = useActionState(addPost, undefined);
+  console.log("userId --------- >>> :", userId)
   return (
-    <div className={styles.container}>AdminPostForm</div>
+    <form action={formAction} className={styles.container}>
+      <h1>Add New Post</h1>
+
+      <input type="hidden" name="userId" value={userId} />
+      <input type="text" name="title" placeholder="Title" />
+      <input type="text" name="slug" placeholder="Slug" />
+      <input type="text" name="img" placeholder="Image" />
+      <textarea type="text" name="desc" placeholder="Desc" rows={10}/>
+      <button>Add</button>
+      {state && state.error}
+    </form>
   )
 }
 
--- src/components/adminUserForm/adminUserForm.jsx
+++ src/components/adminUserForm/adminUserForm.jsx
@@ -1,8 +1,28 @@
+"use client"
+
+import { addUser } from "@/lib/action";
 import styles from "./adminUserForm.module.css";
+import {useActionState} from"react";
 
 const AdminUserForm = () => {
+  const [state, formAction] = useActionState(addUser, undefined);
+
   return (
-    <div className={styles.container}>AdminUserForm</div>
+    <form action={formAction} className={styles.container}>
+      <h1>Add New User</h1>
+
+      <input type="text" name="username" placeholder="username" />
+      <input type="text" name="email" placeholder="email" />
+      <input type="password" name="password" placeholder="password" />
+      <input type="text" name="img" placeholder="Image" />
+      <select name="isAdmin" id="">
+        <option value="false"> Is Admin ?</option>
+        <option value="false"> No</option>
+        <option value="true"> Yes</option>
+      </select>
+      <button>Add</button>
+      {state && state.error}
+    </form>
   )
 }
 
