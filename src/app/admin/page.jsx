dfec35c20d2d2685852107cefd2ae4683e37d10f
FALSAL <coconto.info@gmail.com>
2024-11-21 18:05:43 +0100

    Admin page: create all skeleton folders/files for Users/Posts. create functional adminUsers/Posts files

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,6 +1,34 @@
+import { Suspense } from "react";
+import styles from "./adminPage.module.css";
+import AdminPosts from "@/components/adminPosts/adminPosts";
+import AdminPostForm from "@/components/adminPostForm/adminPostForm";
+import AdminUsers from "@/components/adminUsers/adminUsers";
+import AdminUserForm from "@/components/adminUserForm/adminUserForm";
+
 const Adminpage = () => {
   return (
-    <div>Adminpage</div>
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <AdminPosts/>
+          </Suspense>
+        </div>
+        <div className={styles.col}>
+          <AdminPostForm/>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <AdminUsers/>
+          </Suspense>
+        </div>
+        <div className={styles.col}>
+          <AdminUserForm/>
+        </div>
+      </div>
+    </div>
   )
 }
 
